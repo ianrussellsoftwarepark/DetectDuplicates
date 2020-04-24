69bd767ea343f805a0a8bed6c71eb49d085a61d8
Ian Russell <ian.russell@softwarepark.cc>
2020-04-24 13:41:05 +0100

    initial commit

--- DetectDuplicatesFSharp/Program.fs
+++ DetectDuplicatesFSharp/Program.fs
@@ -1,81 +1,40 @@
-﻿open System.IO
-open System.Linq
-open System.Text.RegularExpressions
+﻿open System.Text.RegularExpressions
+open System.IO
 
-type FileReader = string -> Result<string seq,exn>
-
-let output (data:seq<string*string>) =
-    data 
-    |> Seq.iter (fun (key,value) -> printfn "Duplicate %s found in %s" key value)
-
-let getPath (args: string []) =
-    match args.Any() with
-    | true -> args.[0]
-    | false -> Directory.GetCurrentDirectory()
-
-let getFiles path =
-    printfn "Looking for duplicates in %s" path
-    Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories)
-
-let readFile : FileReader =
-    fun path ->
-        try
-            seq { use reader = new StreamReader(File.OpenRead(path))
-                  while not reader.EndOfStream do
-                      yield reader.ReadLine() }
-            |> Ok
-        with
-        | ex -> Error ex
-        
-let parseLine (fileName:string) (line:string)  =
-    let regexMatch = Regex.Match(line, "PackageReference Include=\"([^\"]*)\"")
-    match regexMatch.Success with
-    | true -> 
-        Some (regexMatch.Groups.[1].Value, fileName)
-    | _ -> None
-
-let parse (fileName:string) (data:string seq) =
-    data
-    |> Seq.map (fun x-> parseLine fileName x)
-    |> Seq.choose id
-    
-    
-let findDuplicates (packages:seq<string * string>) =
-    packages
-    |> Seq.groupBy (fun s -> s)
-    |> Seq.filter (fun (key, values) -> (values.Count() >= 2)) 
-    |> Seq.map (fun(key,_)-> key)
-    
-let checkFile (file: string) =
-    let fileName = Path.GetFileName(file)
-    try
-        match file |> readFile with
-        | Ok data -> data |> parse fileName |> findDuplicates   
-        | Error ex ->   printfn "Error: %A" ex.Message
-                        Seq.empty<string*string>
-    with
-    | ex -> printfn "Error processing entry in %s" fileName
-            Seq.empty<string*string>
-
-let loopFiles (files: string []) =
-    Array.map (fun x -> checkFile x) files
-
-let processProjectFiles (args: string []) =
+let getDirectory (args:string array) =
     args
-    |> getPath
-    |> getFiles
-    |> loopFiles
-    |> Seq.collect (fun x->x)
+    |> Array.tryHead
+    |> Option.defaultValue (Directory.GetCurrentDirectory())
+
+let (|Match|_|) regex str =
+    let m = Regex(regex).Match(str)
+    if m.Success then Some (List.tail [ for x in m.Groups -> x.Value ])
+    else None
+
+let findDuplicates file =
+    file
+    |> File.ReadLines
+    |> Seq.choose (
+        function
+        | Match "<PackageReference Include=\"([^\"]*)\"" [ s ] -> Some s
+        | _ -> None)
+    |> Seq.groupBy id
+    |> Seq.choose (fun (s, x) -> if Seq.length x > 1 then Some s else None)
+
+let processFolder path =
+    printfn "Looking for duplicates in %s" path
+    let files = Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories)
+    let result = ResizeArray<string * string seq>()
+    for file in files do
+        let duplicates = file |> findDuplicates
+        if duplicates |> Seq.isEmpty |> not then result.Add (file |> Path.GetFileName, duplicates)
+    Seq.toList result
 
 [<EntryPoint>]
 let main argv =
-
-    let dups = processProjectFiles argv
-    dups |> output
-
-    printfn "Duplicate detection complete"
-    
-    if dups.Any() then
-        1
-    else
-        0
+    argv
+    |> getDirectory 
+    |> processFolder
+    |> function
+       | [] -> printfn "No duplicates found"; 0
+       | items -> items |> List.iter (fun item -> printfn "%s -> %A" (fst item) (snd item)); 1
